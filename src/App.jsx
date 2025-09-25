6ea1b3ffc20192e4f927be671d65e7b160eef1d8
gladson65 <grauth65@gmail.com>
2025-09-25 23:30:22 +0530

    update the slide or carousel. without any library

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
+import Carousel from './Carousel'
 import './App.css'
 
+
 function App() {
- 
+
+  const slides = [
+    "https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8=",
+    "https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8=",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQykzoZeCE0p7LeuyHnLYCdPP2jju9d5PaMeA&s"
+  ]
 
   return (
     <>
-      
+      <div>
+        <Carousel slides={slides}/>
+      </div>
     </>
   )
 }
